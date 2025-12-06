8d875b2d8d26d05fe2cfb7ddecd9c5b8105d66f6
umaimasif <umaimaasif367@gmail.com>
2025-12-07 04:21:56 +0500

    Update imports and maintain functionality in helper.py

--- helper.py
+++ helper.py
@@ -1,5 +1,7 @@
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
 from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain_core.prompts import PromptTemplate# updated import for v1.1.2
+from langchain.chains import LLMChain
 from pypdf import PdfReader
 import pandas as pd
 import re
@@ -48,4 +50,5 @@ def create_docs(user_pdf_list):
             df = pd.concat([df, pd.DataFrame([data_dict])], ignore_index=True)
 
     return df
+
 
