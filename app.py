3a2032d5c103bb00d0f2de1833dd83693b5a404f
umaimasif <umaimaasif367@gmail.com>
2025-12-07 03:32:05 +0500

    Refactor and update streamlit_bill_extractor.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import pandas as pd
 import re
 import ast
 from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain.prompts import PromptTemplate
+from langchain.prompts.prompt import PromptTemplate
 import os
 from dotenv import find_dotenv, load_dotenv
 
@@ -122,3 +122,4 @@ def main():
 # Run the app
 if __name__ == "__main__":
     main()
+
